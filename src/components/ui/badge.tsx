d808c138a21bd1bcadb6ec99524063a18973daec
akio <124953279+akiomatic108@users.noreply.github.com>
2024-03-23 03:04:45 -0700

    style: adjust padding and rounded corner

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-[4px] border px-2.5 py-0.5 text-sm',
+  'inline-flex items-center rounded border px-2.5 py-1 text-sm',
   {
     variants: {
       variant: {
