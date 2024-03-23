6ab585949f9508691d610e3cb1fe566df4cfad04
Kai <kkai.chien0813@gmail.com>
2024-03-22 21:54:02 -0700

    feat: style button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,24 +5,19 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-xl text-lg font-normal ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 capitalize',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:opacity-80',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:opacity-80',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:opacity-80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        default: 'bg-primary text-white hover:opacity-80',
+        secondary:
+          'border border-primary bg-white text-[#333] hover:bg-[#CBD5E1] hover:border-none ',
+        disable: 'bg-white border border-color-gray-500 text-gray-500',
+        danger: 'bg-rose-600 text-white hover:opacity-80',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        default: 'w-64 h-12 px-4 py-2',
+        sm: 'w-40 h-12 px-3',
       },
     },
     defaultVariants: {
