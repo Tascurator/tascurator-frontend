3da833e8458dc25cd9f9da8c4b548d402ec1c48a
Rina <vvivid0en@gmail.com>
2024-03-26 08:13:54 -0700

    feat: fix globals.css and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,8 +23,21 @@ module.exports = {
         accent: {
           DEFAULT: 'rgb(var(--color-accent))',
         },
+        muted: {
+          DEFAULT: 'rgb(var(--muted))',
+        },
+        popover: {
+          DEFAULT: 'rgb(var(--popover))',
+        },
+        card: {
+          DEFAULT: 'rgb(var(--card))',
+        },
+        background: 'rgb(var(--background))',
         black: 'rgb(var(--color-black))',
         overlay: 'rgb(var(--color-overlay))',
+        input: 'rgb(var(--input))',
+        border: 'rgb(var(--border))',
+        ring: 'rgb(var(--ring))',
       },
     },
   },
