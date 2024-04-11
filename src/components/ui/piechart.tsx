ee537a07337ff8d60c6374e5b906e6cce74c9b74
yuki-92c <jdpmcs@gmail.com>
2024-04-10 18:18:46 -0700

    feat: create UI components pie chart

---
+++ src/components/ui/piechart.tsx
@@ -0,0 +1,82 @@
+function Progress({ progressPercent }: { progressPercent: number }) {
+  // size of the circle
+  const size = 210;
+  // radius of the circle
+  const radius = 50;
+  // circumference of the circle
+  const circumference = 2 * Math.PI * radius;
+  // strokeDashoffset
+  const strokeDashoffset =
+    circumference - (progressPercent / 100) * circumference;
+
+  return (
+    <>
+      <style>
+        {`@keyframes circleStroke {
+          from {
+            stroke-dashoffset: ${circumference};
+          }
+          to {
+            stroke-dashoffset: ${strokeDashoffset};
+          }
+        }
+        @keyframes rotationObject {
+          from {
+            transform: rotate(0deg);
+          }
+          to {
+            transform: rotate(${progressPercent * 3.6}deg)
+          }
+        }
+        `}
+      </style>
+      <div className="relative">
+        <svg
+          viewBox={`0 0 ${size} ${size}`}
+          style={{ transform: 'rotate(-90deg)' }}
+        >
+          <circle
+            r={radius}
+            cx={size / 2}
+            cy={size / 2}
+            stroke="#CBD5E1"
+            strokeWidth="15"
+            fill="transparent"
+            strokeDasharray={circumference}
+          />
+          <circle
+            r={radius}
+            cx={size / 2}
+            cy={size / 2}
+            stroke="#06B6D4"
+            strokeWidth="15"
+            fill="transparent"
+            strokeDasharray={circumference}
+            style={{
+              animation: 'circleStroke 3s ease forwards',
+            }}
+          />
+        </svg>
+        <div
+          className="absolute top-0 left-1/2 "
+          style={{
+            translate: '-50% -50%',
+            transformOrigin: `center ${256 + 126 / 2}px`,
+            animation: 'rotationObject 3s ease forwards',
+          }}
+        ></div>
+        <div className="absolute text-black"></div>
+        <div
+          className="absolute top-1/2 left-1/2 text-4xl font-copperplate text-black italic whitespace-nowrap"
+          style={{
+            translate: '-50% -50%',
+          }}
+        >
+          {progressPercent}%
+        </div>
+      </div>
+    </>
+  );
+}
+
+export { Progress };
