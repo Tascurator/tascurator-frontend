ddee8811c508607ea8f462f43d940cc4e8bda9fe
yuki-92c <jdpmcs@gmail.com>
2024-04-14 17:36:13 -0700

    fix: import modules

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import * as React from 'react';
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { forwardRef, ElementRef, ComponentPropsWithoutRef } from 'react';
+import { Root, Indicator } from '@radix-ui/react-checkbox';
 import { Check } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
-const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+const Checkbox = forwardRef<
+  ElementRef<typeof Root>,
+  ComponentPropsWithoutRef<typeof Root>
 >(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Root
+  <Root
     ref={ref}
     className={cn(
       'peer h-6 w-6 m-1 shrink-0 rounded border-2 border-slate-400 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-light data-[state=checked]:text-white data-[state=checked]:border data-[state=checked]:border-none',
@@ -18,13 +18,11 @@ const Checkbox = React.forwardRef<
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator
-      className={cn('flex items-center justify-center text-current')}
-    >
+    <Indicator className={cn('flex items-center justify-center text-current')}>
       <Check className="h-6 w-6" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
+    </Indicator>
+  </Root>
 ));
-Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+Checkbox.displayName = Root.displayName;
 
 export { Checkbox };
