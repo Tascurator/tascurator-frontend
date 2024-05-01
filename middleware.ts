90ddc3d1ec0aadd08245a6bd454571d36ab8a1bf
akio <124953279+akiomatic@users.noreply.github.com>
2024-05-01 07:53:04 -0700

    feat: setup supabase (#68)

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { type NextRequest } from 'next/server';
+import { updateSession } from '@/utils/supabase/middleware';
+
+export const middleware = async (request: NextRequest) => {
+  // update user's auth session
+  return await updateSession(request);
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)å
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+};
