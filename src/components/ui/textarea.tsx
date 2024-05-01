91cda3951e649d0e97a07fa51b0f1e15d31cc633
Kai <kkai.chien0813@gmail.com>
2024-04-30 21:00:04 -0700

    chore: add margin

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'flex h-[283px] w-full rounded-xl border border-input border-slate-400 bg-white px-3 p-px pb-[18px] text-lg ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex mt-1.5 h-[283px] w-full rounded-xl border border-input border-slate-400 bg-white px-3 p-px pb-[18px] text-lg ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         ref={ref}
