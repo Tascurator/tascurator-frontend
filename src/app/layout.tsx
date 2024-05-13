3641db23f948c79ebaf60d3bac3207f1c288e8f3
Kai <kkai.chien0813@gmail.com>
2024-05-13 16:09:35 -0700

    chore: style the design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
 
 const roboto = Roboto({ weight: ['400', '500'], subsets: ['latin'] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <main>{children}</main>
+        <Toaster />
+      </body>
     </html>
   );
 }
