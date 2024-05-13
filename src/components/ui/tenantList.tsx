f65ae889ef1c07751a04e2b6ad2e5962b896e76e
yuki-92c <jdpmcs@gmail.com>
2024-05-13 13:49:13 -0700

    feat: import dropdown menu

--- src/components/ui/tenantList.tsx
+++ src/components/ui/tenantList.tsx
@@ -1,18 +1,55 @@
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
-import { Ellipsis } from 'lucide-react';
+import { Ellipsis, Trash2, SquarePen } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItemWithIcon,
+} from '@/components/ui/dropdown-menu';
 
-const TenantList = () => {
+interface TenantListProps {
+  fallback: string;
+  name: string;
+}
+
+/**
+ * @example
+ * ```tsx
+ * return (
+ * <TenantList fallback='MM' name='Momo'/>
+ * )
+ */
+const TenantList = ({ fallback, name }: TenantListProps) => {
   return (
     <div className="flex items-center justify-between w-full h-full mx-5 my-3">
       <div>
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
+          <AvatarFallback>{fallback}</AvatarFallback>
         </Avatar>
       </div>
-      <div className="ml-4">name</div>
+      <div className="ml-4">{name}</div>
       <div className="ml-auto">
-        <Ellipsis />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button className="p-4">
+              <Ellipsis />
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              <DropdownMenuItemWithIcon icon={<SquarePen />}>
+                {' '}
+                Edit name{' '}
+              </DropdownMenuItemWithIcon>
+              <DropdownMenuItemWithIcon icon={<Trash2 />}>
+                {' '}
+                Delete{' '}
+              </DropdownMenuItemWithIcon>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
