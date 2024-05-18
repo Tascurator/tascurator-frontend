1e3b7615c22cf10d084c8fb09593d54e2befc8cb
akio <124953279+akiomatic@users.noreply.github.com>
2024-05-17 17:18:51 -0700

    chore: add TSDoc

--- src/components/ui/formMessage.tsx
+++ src/components/ui/formMessage.tsx
@@ -1,11 +1,22 @@
 import { HTMLAttributes } from 'react';
 import { cn } from '@/lib/utils';
 
+/**
+ * FormMessageProps represents the props for the FormMessage component.
+ *
+ * @property {string} message - The message to display
+ */
 export interface IFormMessageProps
   extends HTMLAttributes<HTMLParagraphElement> {
   message: string;
 }
 
+/**
+ * FormMessage is the component used to display form validation messages.
+ *
+ * @example
+ * <FormMessage message="This field is required" />
+ */
 export const FormMessage = ({
   className,
   message,
