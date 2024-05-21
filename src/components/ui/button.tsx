a96ba178cb0823c934d70b9e1e9c8409215456f7
akio <124953279+akiomatic@users.noreply.github.com>
2024-05-20 17:38:48 -0700

    feat: correct imports

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { ButtonHTMLAttributes, forwardRef } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
@@ -36,7 +36,7 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
