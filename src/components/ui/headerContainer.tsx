816fee2f307af2900a631c2b40d4446af688ed0f
SatomiNagano <stm.nagano.wk20@gmail.com>
2024-05-23 19:21:30 -0700

    feat: create header container and items components

--- src/components/ui/headerContainer.tsx
+++ src/components/ui/headerContainer.tsx
@@ -1,7 +1,15 @@
-export default function Header() {
+import { ReactNode } from 'react';
+
+interface HeaderContainerProps {
+  children: ReactNode;
+}
+
+export default function HeaderContainer({ children }: HeaderContainerProps) {
   return (
-    <header className="sticky top-0 z-10 bg-primary text-white">
-      <div className="container flex items-center justify-between h-14 py-4 px-6"></div>
+    <header className="sticky top-0 z-10 bg-primary text-white max-w-screen-sm w-full">
+      <div className="container flex items-center justify-between h-14 py-4 px-4">
+        {children}
+      </div>
     </header>
   );
 }
--- src/components/ui/headerItem.tsx
+++ src/components/ui/headerItem.tsx
@@ -1,4 +1,4 @@
-import { Home } from 'lucide-react';
+import { Ellipsis, Home, LogOutIcon } from 'lucide-react';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,38 +8,113 @@ import {
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItemWithIcon,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+/**
+ * HeaderItemForTop
+ * @description
+ * This component is used to render the top header item.
+ * @example
+ * ```tsx
+ * return (
+ *  <Header>
+ *   <HeaderItemForTop />
+ * </Header>
+ * );
+ * ```
+ *
+ * HeaderItemWithDropDown
+ * @example
+ * ```tsx
+ * const menuItems = [
+ * {title: 'Edit', icon: <SquarePen />},
+ * {title: 'Delete', icon: <Trash2 />},
+ * ];
+ *
+ * return (
+ * <Header>
+ *   <HeaderItemWithDropDown pageTitle="Sample Share House" menuItems={menuItems} />
+ * </Header>
+ * );
+ * ```
+ *
+ * HeaderItemWithoutDropDown
+ * @example
+ * ```tsx
+ * return (
+ * <Header>
+ *  <HeaderItemWithoutDropDown pageTitle="Setup" />
+ * </Header>
+ * );
+ */
+
 function HeaderItemForTop() {
   return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/">
-            <Home className="w-6 h-6" />
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbItem>
-          <BreadcrumbPage>Dashboard</BreadcrumbPage>
-        </BreadcrumbItem>
-      </BreadcrumbList>
-    </Breadcrumb>
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink className="p-2" href="/">
+              <Home className="w-6 h-6" />
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <button className="p-2">
+        <LogOutIcon />
+      </button>
+    </>
   );
 }
 
-function HeaderItemWithDropDown({ pageTitle }: { pageTitle: string }) {
+function HeaderItemWithDropDown({
+  pageTitle,
+  menuItems,
+}: {
+  pageTitle: string;
+  menuItems: { title: string; icon: React.ReactNode }[];
+}) {
   return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/">
-            <Home className="w-6 h-6" />
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>{pageTitle}</BreadcrumbPage>
-        </BreadcrumbItem>
-      </BreadcrumbList>
-    </Breadcrumb>
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem className="pl-1">
+            <BreadcrumbLink className="p-1" href="/">
+              <Home className="w-6 h-6" />
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage className="pl-1">{pageTitle}</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button className="p-2">
+            <Ellipsis />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            {menuItems.map((item, index) => (
+              <DropdownMenuItemWithIcon key={index} icon={item.icon}>
+                {item.title}
+              </DropdownMenuItemWithIcon>
+            ))}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 }
 
@@ -47,14 +122,14 @@ function HeaderItemWithoutDropDown({ pageTitle }: { pageTitle: string }) {
   return (
     <Breadcrumb>
       <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/">
+        <BreadcrumbItem className="pl-1">
+          <BreadcrumbLink className="p-1" href="/">
             <Home className="w-6 h-6" />
           </BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbSeparator />
         <BreadcrumbItem>
-          <BreadcrumbPage>{pageTitle}</BreadcrumbPage>
+          <BreadcrumbPage className="pl-1">{pageTitle}</BreadcrumbPage>
         </BreadcrumbItem>
       </BreadcrumbList>
     </Breadcrumb>
