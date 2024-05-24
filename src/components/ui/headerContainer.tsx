d1374eb6109157dfa4c59d5923bce67d69c9cd14
SatomiNagano <stm.nagano.wk20@gmail.com>
2024-05-23 19:26:54 -0700

    fix: modify interface name

--- src/components/ui/headerContainer.tsx
+++ src/components/ui/headerContainer.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from 'react';
 
-interface HeaderContainerProps {
+interface IHeaderContainerProps {
   children: ReactNode;
 }
 
-export default function HeaderContainer({ children }: HeaderContainerProps) {
+export default function HeaderContainer({ children }: IHeaderContainerProps) {
   return (
     <header className="sticky top-0 z-10 bg-primary text-white max-w-screen-sm w-full">
       <div className="container flex items-center justify-between h-14 py-4 px-4">
