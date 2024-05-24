86679f0b61ad506960a1247e57e40d257dfa013c
akio <124953279+akiomatic@users.noreply.github.com>
2024-05-24 08:30:53 -0700

    feat: list all constants for input texts

---
+++ src/constants/input-texts.ts
@@ -0,0 +1,52 @@
+/**
+ * This file contains all the input texts used in the application.
+ */
+export const INPUT_TEXTS = {
+  // Email
+  EMAIL: {
+    label: 'Email',
+    placeholder: 'Email',
+  },
+
+  // Password
+  PASSWORD: {
+    label: 'Password',
+    placeholder: 'Password',
+  },
+  NEW_PASSWORD: {
+    label: 'New Password',
+    placeholder: 'New Password',
+  },
+
+  // ShareHouse
+  SHAREHOUSE_NAME: {
+    label: 'Share House Name',
+    placeholder: 'Share House Name',
+  },
+
+  // Category
+  CATEGORY_NAME: {
+    label: 'Category Name',
+    placeholder: 'Category Name',
+  },
+
+  // Task
+  TASK_TITLE: {
+    label: 'Task Title',
+    placeholder: 'Task Title',
+  },
+  TASK_DESCRIPTION: {
+    label: 'Task Description',
+    placeholder: 'Task Description',
+  },
+
+  // Tenant
+  TENANT_NAME: {
+    label: 'Tenant Name',
+    placeholder: 'Tenant Name',
+  },
+  TENANT_EMAIL: {
+    label: "Tenant's Email",
+    placeholder: "Tenant's Email",
+  },
+};
