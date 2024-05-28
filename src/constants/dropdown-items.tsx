7e1291233e4424dc49402ed623a2b7816c376fd6
akio <124953279+akiomatic@users.noreply.github.com>
2024-05-27 22:42:37 -0700

    feat: update dropdown list items

--- src/constants/dropdown-items.tsx
+++ src/constants/dropdown-items.tsx
@@ -17,41 +17,47 @@ interface IDropdownItems {
  * This file contains all the dropdown items used in the application.
  */
 export const DROPDOWN_ITEMS: IDropdownItems = {
-  // Add
-  ADD_TASK: {
-    icon: <CirclePlusIcon />,
-    text: 'Add task',
+  // ShareHouse
+  EDIT_SHAREHOUSE_NAME: {
+    icon: <SquarePenIcon />,
+    text: 'Edit share house name',
+  },
+  DELETE_SHAREHOUSE: {
+    icon: <Trash2Icon />,
+    text: 'Delete share house',
   },
 
-  // Delete
-  DELETE: {
-    icon: <Trash2Icon />,
-    text: 'Delete',
+  // Category
+  EDIT_CATEGORY_NAME: {
+    icon: <SquarePenIcon />,
+    text: 'Edit category name',
   },
   DELETE_CATEGORY: {
     icon: <Trash2Icon />,
     text: 'Delete category',
   },
 
-  // Edit
-  EDIT: {
-    icon: <SquarePenIcon />,
-    text: 'Edit',
-  },
-  EDIT_SHAREHOUSE: {
-    icon: <SquarePenIcon />,
-    text: 'Edit share house name',
-  },
-  EDIT_CATEGORY_NAME: {
-    icon: <SquarePenIcon />,
-    text: 'Edit category name',
+  // Task
+  ADD_TASK: {
+    icon: <CirclePlusIcon />,
+    text: 'Add task',
   },
   EDIT_TASK: {
     icon: <NotebookPenIcon />,
     text: 'Edit task',
   },
+  DELETE_TASK: {
+    icon: <Trash2Icon />,
+    text: 'Delete task',
+  },
+
+  // Tenant
   EDIT_TENANT: {
     icon: <SquarePenIcon />,
-    text: 'Edit tenant',
+    text: 'Edit tenant setting',
+  },
+  DELETE_TENANT: {
+    icon: <Trash2Icon />,
+    text: 'Delete tenant',
   },
 };
