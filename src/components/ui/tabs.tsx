6700dad1cdd2cbd28c9c5d9689679c3d90be238d
akio <124953279+akiomatic@users.noreply.github.com>
2024-05-27 23:01:35 -0700

    chore: add TSDoc to `Tabs` component

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -3,8 +3,36 @@ import { forwardRef, ElementRef, ComponentPropsWithoutRef } from 'react';
 import { Root, List, Trigger, Content } from '@radix-ui/react-tabs';
 import { cn } from '@/lib/utils';
 
+/**
+ * The root component of the Tabs.
+ * The usage is the same as the official documentation of shadcn/ui.
+ *
+ * @example
+ * <Tabs defaultValue="tab-1">
+ *   <TabsList>
+ *     <TabsTrigger value="tab-1">Tab 1</TabsTrigger>
+ *     <TabsTrigger value="tab-2">Tab 2</TabsTrigger>
+ *     <TabsTrigger value="tab-3">Tab 3</TabsTrigger>
+ *   </TabsList>
+ *   <TabsContent value="tab-1">Content 1</TabsContent>
+ *   <TabsContent value="tab-2">Content 2</TabsContent>
+ *   <TabsContent value="tab-3">Content 3</TabsContent>
+ * </Tabs>
+ * @see {@link https://ui.shadcn.com/docs/components/tabs | Tabs}
+ */
 const Tabs = Root;
 
+/**
+ * The list component of the Tabs.
+ * It should be used as a wrapper for the TabsTrigger components.
+ *
+ * @example
+ * <TabsList>
+ *   <TabsTrigger value="tab-1">Tab 1</TabsTrigger>
+ *   <TabsTrigger value="tab-2">Tab 2</TabsTrigger>
+ *   <TabsTrigger value="tab-3">Tab 3</TabsTrigger>
+ * </TabsList>
+ */
 const TabsList = forwardRef<
   ElementRef<typeof List>,
   ComponentPropsWithoutRef<typeof List>
@@ -20,6 +48,13 @@ const TabsList = forwardRef<
 ));
 TabsList.displayName = List.displayName;
 
+/**
+ * The trigger component of the Tabs.
+ * It should be used as a child of the TabsList component.
+ *
+ * @example
+ * <TabsTrigger value="tab-1">Tab 1</TabsTrigger>
+ */
 const TabsTrigger = forwardRef<
   ElementRef<typeof Trigger>,
   ComponentPropsWithoutRef<typeof Trigger>
@@ -35,6 +70,14 @@ const TabsTrigger = forwardRef<
 ));
 TabsTrigger.displayName = Trigger.displayName;
 
+/**
+ * The content component of the Tabs.
+ * It should be used as a child of the Tabs component for each tab.
+ * The value prop should match the value prop of the TabsTrigger component.
+ *
+ * @example
+ * <TabsContent value="tab-1">Content 1</TabsContent>
+ */
 const TabsContent = forwardRef<
   ElementRef<typeof Content>,
   ComponentPropsWithoutRef<typeof Content>
