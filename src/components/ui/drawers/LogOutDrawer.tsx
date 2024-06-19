017ceace1b9bc6215d341215f640bb494214cabf
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-06-19 10:27:23 -0700

    feat: create logout drawer

---
+++ src/components/ui/drawers/LogOutDrawer.tsx
@@ -0,0 +1,72 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { Button } from '@/components/ui/button';
+
+interface ILogOutDrawer {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+}
+
+/**
+ * A drawer component to log out the user
+ *
+ * @param open - The state of the drawer
+ * @param setOpen - The function to set the state of the drawer
+ *
+ * @example
+ * const [open, setOpen] = useState(false);
+ *
+ * <LogOutDrawer open={open} setOpen={setOpen} />
+ *
+ */
+
+export const LogOutDrawer = ({ open, setOpen }: ILogOutDrawer) => {
+  //TODO: implement logout function
+  const handleLogout = () => {
+    setOpen(false);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger />
+      <DrawerContent asChild>
+        <form onSubmit={handleLogout}>
+          <DrawerTitle>Log out?</DrawerTitle>
+          <DrawerDescription>
+            <p className={'mt-2.5'}>Are you sure you want to log out?</p>
+          </DrawerDescription>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button
+                type={'button'}
+                variant={'secondary'}
+                onClick={handleClose}
+                className={'flex-1'}
+              >
+                Cancel
+              </Button>
+            </DrawerClose>
+            <Button
+              type={'submit'}
+              variant={'outline-destructive'}
+              className={'flex-1'}
+            >
+              Log Out
+            </Button>
+          </DrawerFooter>
+        </form>
+      </DrawerContent>
+    </Drawer>
+  );
+};
