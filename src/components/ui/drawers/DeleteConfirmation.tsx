555cf771d791ab61ee9aa26847f8667f13719859
yuki-92c <jdpmcs@gmail.com>
2024-06-19 15:32:07 -0700

    feat: add delete confirmation drawer

---
+++ src/components/ui/drawers/DeleteConfirmation.tsx
@@ -0,0 +1,55 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+
+export interface IDeleteConfirmationProps {
+  deleteItem: string;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+export const DeleteConfirmation = ({
+  deleteItem,
+  open,
+  setOpen,
+}: IDeleteConfirmationProps) => {
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger />
+      <DrawerContent>
+        <DrawerClose />
+        <DrawerTitle>Confirm delete</DrawerTitle>
+        <DrawerDescription>
+          {`Are you sure you want to delete ${deleteItem}?`}
+          <div className="flex items-center content-start pt-4">
+            <Checkbox />
+            <p>{`Yes, I want to delete ${deleteItem}.`}</p>
+          </div>
+        </DrawerDescription>
+        <DrawerFooter>
+          <div className="flex-1">
+            <DrawerClose asChild>
+              <Button
+                type={'button'}
+                variant={'secondary'}
+                className={'flex-1'}
+              >
+                Cancel
+              </Button>
+            </DrawerClose>
+          </div>
+          <div className="flex-1">
+            <Button variant="destructive">Delete</Button>
+          </div>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
