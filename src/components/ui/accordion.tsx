0ad63797ee943d69ff534f069198eb073b65c740
Rina Hara <77369174+HrRn@users.noreply.github.com>
2024-06-21 09:57:52 -0700

    feat: fix accordion component (#170)

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -21,9 +21,20 @@ import { cn } from '@/lib/utils';
  *
  * <Accordion type="single" collapsible className="w-full">
  *   <AccordionItem value="item-1">
- *     <AccordionTrigger scheduleDate={"12/31-01/07"}>Bathroom
- *        <CircleEllipsis className='ml-4'/>
+ *
+ *     // ScheduleDate
+ *     <AccordionTrigger scheduleDate={"12/31-01/07"}>
+ *        Bathroom
  *     </AccordionTrigger>
+ *
+ *     // CircleEllipsis
+ *     <div className="flex items-center w-full">
+ *       <AccordionTrigger>Bathroom</AccordionTrigger>
+ *         <div className="flex justify-center items-center p-4 cursor-pointer">
+ *           <CircleEllipsis />
+ *         </div>
+ *     </div>
+
  *     <AccordionContent>
  *        // content
  *     </AccordionContent>
@@ -55,7 +66,7 @@ const AccordionItem = forwardRef<
   <Item
     ref={ref}
     className={cn(
-      'border-b bg-primary-lightest rounded-xl p-4 shadow-md m-3',
+      'border-b bg-primary-lightest rounded-xl shadow-md m-3',
       className,
     )}
     {...props}
@@ -73,22 +84,22 @@ const AccordionTrigger = forwardRef<
   ElementRef<typeof Trigger>,
   AccordionTriggerProps
 >(({ className, children, scheduleDate, ...props }, ref) => (
-  <Header className="flex">
+  <Header className="flex justify-center items-center w-full">
     <Trigger
       ref={ref}
       className={cn(
-        'flex flex-1 items-center font-medium transition-all [&[data-state=open]>svg]:rotate-180',
+        'flex flex-1 justify-between items-center font-medium text-left p-4 transition-all [&[data-state=open]>svg]:rotate-180',
         className,
       )}
       {...props}
     >
+      <ChevronDown className="h-4 w-4 mr-2 shrink-0 transition-transform duration-200" />
       {children}
       <div className="flex-1" />
       {/* display only scheduleDate is set*/}
       {scheduleDate && (
-        <p className="text-base leading-7 text-black">{scheduleDate}</p>
+        <p className="text-sm leading-7 text-black">{scheduleDate}</p>
       )}
-      <ChevronDown className="h-4 w-4 ml-1.5 shrink-0 transition-transform duration-200" />
     </Trigger>
   </Header>
 ));
@@ -108,7 +119,7 @@ const AccordionContent = forwardRef<
 >(({ className, children, ...props }, ref) => (
   <Content
     ref={ref}
-    className="overflow-hidden transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down bg-white rounded-xl mt-2"
+    className="overflow-hidden transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down bg-white rounded-xl mt-2 mb-4 mx-4"
     {...props}
   >
     <div className={cn('p-4', className)}>{children}</div>
