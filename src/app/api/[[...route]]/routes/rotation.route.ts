2f1bead97b708c09a83d246e13e25150cdb3fe30
Kai <kkai.chien0813@gmail.com>
2024-06-21 16:28:14 -0700

    feat: fetch data for current rotation

--- src/app/api/[[...route]]/routes/rotation.route.ts
+++ src/app/api/[[...route]]/routes/rotation.route.ts
@@ -1,14 +1,43 @@
+import { PrismaClient } from '@prisma/client';
 import { Hono } from 'hono';
 
 const app = new Hono();
+const prisma = new PrismaClient();
 
 export default app;
 
-app.get('/current/:shareHouseId', (c) => {
+app.get('/current/:shareHouseId', async (c) => {
   const shareHouseId = c.req.param('shareHouseId');
-  return c.json({
-    message: `Share house id for current rotation: ${shareHouseId}`,
-  });
+  try {
+    // const shareHouse = await prisma.shareHouse.findUnique({
+    // 	where: {
+    // 		id: shareHouseId,
+    // 	},
+    // 	select: {
+    // 		assignmentSheetId: true,
+    // 	},
+    // });
+    const shareHouseWithAssignmentSheet = await prisma.shareHouse.findUnique({
+      where: {
+        id: shareHouseId,
+      },
+      include: {
+        assignmentSheet: true,
+      },
+    });
+
+    if (
+      !shareHouseWithAssignmentSheet ||
+      !shareHouseWithAssignmentSheet.assignmentSheet
+    ) {
+      return c.json({ error: 'ShareHouse or AssignmentSheet not found' }, 404);
+    }
+
+    return c.json(shareHouseWithAssignmentSheet?.assignmentSheet.assignedData);
+  } catch (error) {
+    console.error(error);
+    return c.json({ error: 'An error occurred while fetching data' }, 500);
+  }
 });
 
 app.get('/next/:shareHouseId', (c) => {
