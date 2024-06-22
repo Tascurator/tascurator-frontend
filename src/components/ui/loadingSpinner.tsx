8becc5eea4093f2b815eb4d773d816925a689642
Rina <vvivid0en@gmail.com>
2024-06-21 21:53:53 -0700

    feat: fix LoadingSpinner component

--- src/components/ui/loadingSpinner.tsx
+++ src/components/ui/loadingSpinner.tsx
@@ -1,26 +1,31 @@
+'use client';
+import { useLockBodyScroll } from '@/hooks/useLockBodyScroll';
+
 /**
  *
  * A Loading spinner component to show while data is updating
  *
  * @example
- * layout.tsx
  *
- * <html lang="en">
- *   <body className={cn('flex justify-center items-start w-screen min-h-screen bg-primary-lightest', roboto.className,)}>
- *     <LoadingSpinner />
- *     <main className={'max-w-screen-sm min-h-screen w-full h-full bg-white px-6'}>
- *       {children}
- *     </main>
- *     <Toaster />
- *   </body>
- * </html>
+ * <LoadingSpinner isLoading={false} />
  *
  */
 
-export const LoadingSpinner = () => {
+interface LoadingSpinnerProps {
+  isLoading: boolean;
+}
+
+export const LoadingSpinner = ({ isLoading }: LoadingSpinnerProps) => {
+  return <>{isLoading && <LoadingOverlay />}</>;
+};
+
+export const LoadingOverlay = () => {
+  useLockBodyScroll();
   return (
-    <div className="fixed z-50 bg-black/50 w-full h-full flex justify-center items-center">
-      <div className="border-gray-200 h-12 w-12 animate-spin rounded-full border-4 border-t-primary" />
-    </div>
+    <>
+      <div className="fixed left-0 top-0 z-50 bg-black/50 w-full h-full flex justify-center items-center">
+        <div className=" border-gray-200 h-12 w-12 animate-spin rounded-full border-4 border-t-primary" />
+      </div>
+    </>
   );
 };
