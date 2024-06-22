3313b502a9013c8a654873cc06aa818425cb7ede
Kai <kkai.chien0813@gmail.com>
2024-06-22 01:13:23 -0700

    feat: fetch data for edit page

--- src/app/api/[[...route]]/routes/sharehouse.route.ts
+++ src/app/api/[[...route]]/routes/sharehouse.route.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client';
 
 export const app = new Hono();
+const prisma = new PrismaClient();
 
 export default app;
 
@@ -8,9 +10,42 @@ app.get('/', (c) => {
   return c.json({ message: 'share house route' });
 });
 
-app.get('/:shareHouseId', (c) => {
+app.get('/:shareHouseId', async (c) => {
   const shareHouseId = c.req.param('shareHouseId');
-  return c.json({ message: `Share house id: ${shareHouseId}` });
+
+  try {
+    const sharehouseWithOtherTables = await prisma.shareHouse.findUnique({
+      where: {
+        id: shareHouseId,
+      },
+      include: {
+        RotationAssignment: {
+          include: {
+            tenantPlaceholders: {
+              include: {
+                tenant: true,
+              },
+            },
+            categories: {
+              include: {
+                tasks: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!sharehouseWithOtherTables) {
+      return c.json({ error: 'ShareHouse not found' }, 404);
+    }
+
+    return c.json(sharehouseWithOtherTables);
+  } catch (error) {
+    console.error(error);
+    return c.json({ error: 'An error occurred while fetching data' }, 500);
+  }
+  // return c.json({ message: `Share house id: ${shareHouseId}` });
 });
 
 app.patch('/:shareHouseId', (c) => {
