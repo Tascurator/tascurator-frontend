fad96bc334043be797b9c9cedf9500243f668c4c
Rina Hara <77369174+HrRn@users.noreply.github.com>
2024-07-09 11:02:39 -0700

    feat: create pages group3 share houses edit page (#192)
    * feat: create pages: Group3 Share houses Edit Page
    
    * feat: modify TaskCreationDrawer.tsx
    
    * feat: fix share houses edit page
    
    * feat: add toast
    
    * feat: fix TaskCreationDrawer.tsx
    
    * feat: fix loading
    
    * fix: correct import statement for Header in sharehouses/layout.tsx
    
    * fix: fix share house edit page
    
    * fix: move the component file

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -176,7 +176,7 @@ interface IHeaderContainerProps {
 
 const Header = ({ type, pageTitle }: IHeaderContainerProps) => {
   return (
-    <header className="sticky top-0 z-10 bg-primary text-white max-w-screen-sm w-full">
+    <header className="bg-primary text-white max-w-screen-sm w-full">
       <div className="container flex items-center justify-between h-14 py-4 px-4">
         {type === 'HeaderItemForTop' ? (
           <HeaderItemForTop />
