0441373976945a990278feb4111e0970f8524f31
Kai <kkai.chien0813@gmail.com>
2024-07-10 16:23:29 -0700

    feat: create new category and task

--- src/app/api/[[...route]]/routes/category.route.ts
+++ src/app/api/[[...route]]/routes/category.route.ts
@@ -1,4 +1,8 @@
 import { Hono } from 'hono';
+import { zValidator } from '@hono/zod-validator';
+
+import { categoryCreationSchema } from '@/constants/schema';
+import prisma from '@/lib/prisma';
 
 const app = new Hono();
 
@@ -14,9 +18,51 @@ app.delete('/:categoryId', (c) => {
   return c.json({ message: `Deleting category id: ${categoryId}` });
 });
 
-app.post('/:shareHouseId', (c) => {
-  const shareHouseId = c.req.param('shareHouseId');
-  return c.json({
-    message: `Creating share house id for category: ${shareHouseId}`,
-  });
-});
+app.post(
+  '/:shareHouseId',
+  zValidator('json', categoryCreationSchema.omit({ descriptionCount: true })),
+  async (c) => {
+    try {
+      const shareHouseId = c.req.param('shareHouseId');
+      const data = c.req.valid('json');
+
+      const rotationAssignment = await prisma.rotationAssignment.findUnique({
+        where: {
+          shareHouseId: shareHouseId,
+        },
+      });
+
+      if (!rotationAssignment) {
+        return c.json(
+          { error: 'RotationAssignment not found for the given shareHouseId' },
+          404,
+        );
+      }
+
+      const newCategory = await prisma.category.create({
+        data: {
+          name: data.category,
+          rotationAssignment: {
+            connect: {
+              id: rotationAssignment.id,
+            },
+          },
+          tasks: {
+            create: {
+              title: data.title,
+              description: data.description,
+            },
+          },
+        },
+      });
+
+      return c.json(newCategory, 201);
+    } catch (error) {
+      console.error(error);
+      return c.json(
+        { error: 'An error occurred while creating the category' },
+        500,
+      );
+    }
+  },
+);
