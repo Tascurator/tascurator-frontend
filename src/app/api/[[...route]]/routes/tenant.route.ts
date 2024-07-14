a560a168e2e479fdbf4ff0349c9ea8f7de8372b3
Kai <kkai.chien0813@gmail.com>
2024-07-14 10:30:24 -0700

    feat: update tenant name

--- src/app/api/[[...route]]/routes/tenant.route.ts
+++ src/app/api/[[...route]]/routes/tenant.route.ts
@@ -9,10 +9,38 @@ const app = new Hono();
 
 export default app;
 
-app.patch('/:tenantId', (c) => {
-  const tenantId = c.req.param('tenantId');
-  return c.json({ message: `Updating tenant id: ${tenantId}` });
-});
+app.patch(
+  '/:tenantId',
+  zValidator('json', tenantInvitationSchema.pick({ name: true })),
+  async (c) => {
+    try {
+      const tenantId = c.req.param('tenantId');
+      const data = c.req.valid('json');
+
+      const tenant = await prisma.tenant.findUnique({
+        where: {
+          id: tenantId,
+        },
+      });
+
+      if (!tenant) return c.json({ error: 'Tenant not found' }, 404);
+
+      const updateTenant = await prisma.tenant.update({
+        where: {
+          id: tenantId,
+        },
+        data: {
+          name: data.name,
+        },
+      });
+
+      return c.json(updateTenant, 201);
+    } catch (error) {
+      console.error('Error updating tenant:', error);
+      return c.json({ error: 'An error occurred while updating  tenant' }, 500);
+    }
+  },
+);
 
 app.delete('/:tenantId', (c) => {
   const tenantId = c.req.param('tenantId');
