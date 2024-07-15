94c022f4556991ba3414f1a1647f15a4ca96a1bc
akio <124953279+akiomatic@users.noreply.github.com>
2024-07-15 15:14:13 -0700

    feat: add extra 0 when single digit

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -82,8 +82,8 @@ export const convertToPDT = (date: Date): Date => {
  */
 export const formatDate = (date: Date): string => {
   const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
 
   return `${year}/${month}/${day}`;
 };
