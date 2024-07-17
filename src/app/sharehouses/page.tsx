a16ea7fe0ab836b46a8c51a7b9198125fdbef698
yuki-92c <jdpmcs@gmail.com>
2024-07-16 20:37:11 -0700

    fix: add no tenants

--- src/app/sharehouses/page.tsx
+++ src/app/sharehouses/page.tsx
@@ -2,48 +2,78 @@
 import { Progress } from '@/components/ui/piechart';
 import { LandlordDashboard } from '@/components/landlord-dashboard/Dropdown';
 import Link from 'next/link';
+import { FloatingActionButton } from '@/components/ui/floatingActionButton';
+
+interface IShareHouse {
+  shareHouseId: string;
+  shareHouseName: string;
+  progressPercent: number;
+}
 
 const ShareHousesPage = () => {
-  const shareHouses = [
-    {
-      shareHouseId: 'yourShareHouseId',
-      shareHouseName: 'Sample Share House',
-      progressPercent: 50,
-    },
-    {
-      shareHouseId: 'yourShareHouseId2',
-      shareHouseName: 'Sample Share House 2',
-      progressPercent: 80,
-    },
-    {
-      shareHouseId: 'yourShareHouseId3',
-      shareHouseName: 'Sample Share House 3',
-      progressPercent: 20,
-    },
+  const shareHouses: IShareHouse[] = [
+    // {
+    //   shareHouseId: 'yourShareHouseId',
+    //   shareHouseName: 'Sample Share House',
+    //   progressPercent: 50,
+    // },
+    // {
+    //   shareHouseId: 'yourShareHouseId2',
+    //   shareHouseName: 'Sample Share House 2',
+    //   progressPercent: 80,
+    // },
+    // {
+    //   shareHouseId: 'yourShareHouseId3',
+    //   shareHouseName: 'Sample Share House 3',
+    //   progressPercent: 20,
+    // },
   ];
 
-  return (
-    <div>
-      {shareHouses.map((shareHouse) => (
-        <div
-          className="bg-primary-lightest rounded-md my-4 px-4 pt-4 shadow-lg"
-          key={shareHouse.shareHouseId}
-        >
-          <Link href={`/sharehouses/${shareHouse.shareHouseId}/`}>
-            <div className="flex justify-center">
-              <div className="w-32 sm:w-48">
-                <Progress progressPercent={shareHouse.progressPercent} />
-              </div>
-            </div>
-          </Link>
-          <LandlordDashboard
-            shareHouseName={shareHouse.shareHouseName}
-            shareHouseId={shareHouse.shareHouseId}
-          />
+  if (shareHouses.length === 0) {
+    return (
+      <>
+        <div className="flex items-center justify-end mt-2">0/10</div>
+        <div className="flex items-center justify-center mt-4">
+          No share houses
         </div>
-      ))}
-    </div>
-  );
+        <div className="absolute right-11 bottom-8">
+          <FloatingActionButton />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <div className="flex items-center justify-end mt-2">
+          {shareHouses.length}/10
+        </div>
+        <div className="absolute right-11 bottom-8">
+          <FloatingActionButton />
+        </div>
+        {shareHouses.map((shareHouse) => (
+          <div
+            className="bg-primary-lightest rounded-md my-4 px-4 pt-4 shadow-lg"
+            key={shareHouse.shareHouseId}
+          >
+            <Link href={`/sharehouses/${shareHouse.shareHouseId}/`}>
+              <div className="flex justify-center">
+                <div className="w-32 sm:w-48">
+                  <Progress progressPercent={shareHouse.progressPercent} />
+                </div>
+              </div>
+            </Link>
+            <LandlordDashboard
+              shareHouseName={shareHouse.shareHouseName}
+              shareHouseId={shareHouse.shareHouseId}
+            />
+          </div>
+        ))}
+        {/* <div className='fixed bottom-5 right-5 sm:'>
+        <FloatingActionButton />
+      </div> */}
+      </div>
+    );
+  }
 };
 
 export default ShareHousesPage;
