8b4b6e91a3d9b99a0149e8ee9ab65530ba4918bd
akio <124953279+akiomatic@users.noreply.github.com>
2024-07-19 12:07:23 -0700

    feat: set up RPC (#254)
    * chore: add new environment variable in example
    
    * feat: enable RPC
    
    * refactor: restructure every route due to RPC
    
    * feat: add Hono client helper for RPC
    
    * fix: correct error type

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -10,8 +10,16 @@ import categoryRoute from './routes/category.route';
 import taskRoute from './routes/task.route';
 import tenantRoute from './routes/tenant.route';
 
-const app = new Hono().basePath('/api');
+const app = new Hono();
 
+const routes = app
+  .basePath('/api')
+  .route('/sharehouse', sharehouseRoute)
+  .route('/sharehouses', sharehousesRoute)
+  .route('/rotation', rotationRoute)
+  .route('/category', categoryRoute)
+  .route('/task', taskRoute)
+  .route('/tenant', tenantRoute)
 const defaultRoutes = [
   { path: '/sharehouse', route: sharehouseRoute },
   { path: '/sharehouses', route: sharehousesRoute },
@@ -41,34 +49,37 @@ export const authRoutes: string[] = ['/login', '/signup'];
  */
 export const DEFAULT_LOGIN_REDIRECT: string = '/sharehouses';
 
-/**
- * This is a test route to check if the user is logged in.
- * TODO: Remove this route before deploying to production.
- */
-app.get('/whoami', async (c) => {
-  const session = await auth();
+  /**
+   * This is a test route to check if the user is logged in.
+   * TODO: Remove this route before deploying to production.
+   */
+  .get('/whoami', async (c) => {
+    const session = await auth();
 
-  if (!session) {
-    return c.json({
-      message: 'You are not logged in!',
+    if (!session) {
+      return c.json(
+        {
+          error: 'You are not logged in!',
+        },
+        401,
+      );
+    }
+
+    const landlord = await prisma.landlord.findUnique({
+      where: { id: session.user.id },
     });
-  }
 
-  const landlord = await prisma.landlord.findUnique({
-    where: { id: session.user.id },
+    return c.json({
+      message: 'You are logged in!',
+      session,
+      landlord,
+    });
   });
 
-  return c.json({
-    message: 'You are logged in!',
-    session,
-    landlord,
-  });
-});
-
-for (const route of defaultRoutes) {
-  console.log(`Adding route: /api${route.path}`);
-  app.route(`${route.path}`, route.route);
-}
+/**
+ * Expose the type of the api routes.
+ */
+export type AppType = typeof routes;
 
 /**
  * Expose HTTP methods for the app.
--- src/app/api/[[...route]]/routes/rotation.route.ts
+++ src/app/api/[[...route]]/routes/rotation.route.ts
@@ -7,38 +7,96 @@ import type { IAssignedData } from '@/types/server';
 import { AssignedData } from '@/services/AssignedData';
 import { addDays } from '@/utils/dates';
 
-const app = new Hono();
+const app = new Hono()
 
-export default app;
+  /**
+   * Retrieves a list of all categories with who they are assigned to for the current rotation cycle of a sharehouse by its ID
+   * @route GET /api/rotation/current/:shareHouseId
+   */
+  .get('/current/:shareHouseId', async (c) => {
+    const shareHouseId = c.req.param('shareHouseId');
+    try {
+      const shareHouseWithAssignmentSheet = await prisma.shareHouse.findUnique({
+        where: {
+          id: shareHouseId,
+        },
+        include: {
+          assignmentSheet: true,
+        },
+      });
 
-app.get('/current/:shareHouseId', async (c) => {
-  const shareHouseId = c.req.param('shareHouseId');
-  try {
-    const shareHouseWithAssignmentSheet = await prisma.shareHouse.findUnique({
-      where: {
-        id: shareHouseId,
-      },
-      include: {
-        assignmentSheet: true,
-      },
-    });
+      if (
+        !shareHouseWithAssignmentSheet ||
+        !shareHouseWithAssignmentSheet.assignmentSheet
+      ) {
+        return c.json(
+          { error: 'ShareHouse or AssignmentSheet not found' },
+          404,
+        );
+      }
 
-    if (
-      !shareHouseWithAssignmentSheet ||
-      !shareHouseWithAssignmentSheet.assignmentSheet
-    ) {
-      return c.json({ error: 'ShareHouse or AssignmentSheet not found' }, 404);
-    }
+      const assignedData = shareHouseWithAssignmentSheet.assignmentSheet
+        .assignedData as unknown as IAssignedData;
 
-    const assignedData = shareHouseWithAssignmentSheet.assignmentSheet
-      .assignedData as unknown as IAssignedData;
+      const areAllTenantsNull = assignedData.assignments.every(
+        (assignment) => assignment.tenant === null,
+      );
 
-    const areAllTenantsNull = assignedData.assignments.every(
-      (assignment) => assignment.tenant === null,
-    );
+      if (areAllTenantsNull) {
+        return c.json({
+          name: shareHouseWithAssignmentSheet.name,
+          startDate:
+            shareHouseWithAssignmentSheet.assignmentSheet.startDate.toISOString(),
+          endDate: addDays(
+            shareHouseWithAssignmentSheet.assignmentSheet.endDate,
+            -1,
+          ).toISOString(),
+          progressRate: 0,
+          categories: null,
+        });
+      }
 
-    if (areAllTenantsNull) {
-      return c.json({
+      let totalTasks = 0;
+      let totalCompletedTasks = 0;
+
+      const categories = assignedData.assignments.map((assignment) => {
+        const maxTasks = assignment.tasks ? assignment.tasks.length : null;
+        const completedTasks = assignment.tasks
+          ? assignment.tasks.filter((task) => task.isCompleted === true).length
+          : null;
+
+        if (maxTasks !== null) totalTasks += maxTasks;
+        if (completedTasks !== null) totalCompletedTasks += completedTasks;
+
+        const categoryId = assignment.category ? assignment.category.id : null;
+        const categoryName = assignment.category
+          ? assignment.category.name
+          : null;
+
+        if (!assignment.tenant)
+          return c.json({ error: 'Internal Server Error' }, 500);
+
+        const tenantId = assignment.tenant.id;
+        const tenantName = assignment.tenant.name;
+
+        return {
+          id: categoryId,
+          name: categoryName,
+          maxTasks: maxTasks,
+          completedTasks: completedTasks,
+          tenant: {
+            id: tenantId,
+            name: tenantName,
+          },
+        };
+      });
+
+      const progressRate =
+        totalTasks > 0
+          ? ((totalCompletedTasks / totalTasks) * 100).toFixed(1)
+          : 0;
+
+      const currentRotationData = {
         name: shareHouseWithAssignmentSheet.name,
         startDate:
           shareHouseWithAssignmentSheet.assignmentSheet.startDate.toISOString(),
@@ -46,198 +104,155 @@ app.get('/current/:shareHouseId', async (c) => {
           shareHouseWithAssignmentSheet.assignmentSheet.endDate,
           -1,
         ).toISOString(),
-        progressRate: 0,
-        categories: null,
-      });
-    }
-
-    let totalTasks = 0;
-    let totalCompletedTasks = 0;
-
-    const categories = assignedData.assignments.map((assignment) => {
-      const maxTasks = assignment.tasks ? assignment.tasks.length : null;
-      const completedTasks = assignment.tasks
-        ? assignment.tasks.filter((task) => task.isCompleted === true).length
-        : null;
-
-      if (maxTasks !== null) totalTasks += maxTasks;
-      if (completedTasks !== null) totalCompletedTasks += completedTasks;
-
-      const categoryId = assignment.category ? assignment.category.id : null;
-      const categoryName = assignment.category
-        ? assignment.category.name
-        : null;
-
-      if (!assignment.tenant)
-        return c.json({ error: 'Internal Server Error' }, 500);
-
-      const tenantId = assignment.tenant.id;
-      const tenantName = assignment.tenant.name;
-
-      return {
-        id: categoryId,
-        name: categoryName,
-        maxTasks: maxTasks,
-        completedTasks: completedTasks,
-        tenant: {
-          id: tenantId,
-          name: tenantName,
-        },
+        progressRate: progressRate,
+        categories: categories,
       };
-    });
 
-    const progressRate =
-      totalTasks > 0
-        ? ((totalCompletedTasks / totalTasks) * 100).toFixed(1)
-        : 0;
-
-    const currentRotationData = {
-      name: shareHouseWithAssignmentSheet.name,
-      startDate:
-        shareHouseWithAssignmentSheet.assignmentSheet.startDate.toISOString(),
-      endDate: addDays(
-        shareHouseWithAssignmentSheet.assignmentSheet.endDate,
-        -1,
-      ).toISOString(),
-      progressRate: progressRate,
-      categories: categories,
-    };
-
-    return c.json(currentRotationData);
-  } catch (error) {
-    console.error(error);
-    return c.json({ error: 'An error occurred while fetching data' }, 500);
-  }
-});
-
-app.get('/next/:shareHouseId', async (c) => {
-  const shareHouseId = c.req.param('shareHouseId');
-
-  try {
-    const sharehouse = await prisma.shareHouse.findUnique({
-      where: {
-        id: shareHouseId,
-      },
-      include: {
-        RotationAssignment: {
-          select: {
-            rotationCycle: true,
-            categories: {
-              include: {
-                tasks: true,
-              },
-            },
-            tenantPlaceholders: {
-              include: {
-                tenant: true,
-              },
-            },
-          },
-        },
-        assignmentSheet: true,
-      },
-    });
-
-    /**
-     * Return 404 if sharehouse not found
-     */
-    if (!sharehouse) {
-      return c.json({ error: 'ShareHouse not found' }, 404);
+      return c.json(currentRotationData);
+    } catch (error) {
+      console.error(error);
+      return c.json({ error: 'An error occurred while fetching data' }, 500);
     }
+  })
 
-    /**
-     * Return 500 if RotationAssignment or AssignmentSheet not found
-     */
-    if (!sharehouse.RotationAssignment || !sharehouse.assignmentSheet) {
-      return c.json({ error: 'Internal Server Error' }, 500);
-    }
+  /**
+   * Retrieves a list of all categories with who they are assigned to for the next rotation cycle of a sharehouse by its ID
+   * @route GET /api/rotation/next/:shareHouseId
+   */
+  .get('/next/:shareHouseId', async (c) => {
+    const shareHouseId = c.req.param('shareHouseId');
 
-    /**
-     * Create an AssignedData instance for the current rotation cycle.
-     */
-    const assignedData = new AssignedData(
-      sharehouse.assignmentSheet.assignedData as unknown as IAssignedData,
-      sharehouse.assignmentSheet.startDate,
-      sharehouse.assignmentSheet.endDate,
-    );
-
-    /**
-     * Generate the next rotation's assigned data.
-     */
-    const nextAssignedData = assignedData.createNextRotation(
-      sharehouse.RotationAssignment.categories,
-      sharehouse.RotationAssignment.tenantPlaceholders,
-      sharehouse.RotationAssignment.rotationCycle,
-    );
-
-    /**
-     * Structure the nextAssignedData to be returned.
-     */
-    const nextAssignmentData = {
-      name: sharehouse.name,
-      startDate: nextAssignedData.getStartDate().toISOString(),
-      endDate: addDays(nextAssignedData.getEndDate(), -1).toISOString(),
-      categories: nextAssignedData
-        .getAssignments()
-        .map((assignment) => {
-          if (!assignment.tenant) {
-            return null;
-          }
-
-          return {
-            id: assignment.category?.id ?? null,
-            name: assignment.category?.name ?? null,
-            tenant: {
-              id: assignment.tenant.id,
-              name: assignment.tenant.name,
-            },
-          };
-        })
-        .filter((assignment) => assignment !== null),
-    };
-
-    return c.json(nextAssignmentData);
-  } catch (error) {
-    console.error(error);
-    return c.json({ error: 'An error occurred while fetching data' }, 500);
-  }
-});
-
-app.patch(
-  '/:shareHouseId',
-  zValidator('json', rotationCycleUpdateSchema),
-  async (c) => {
     try {
-      const shareHouseId = c.req.param('shareHouseId');
-      const data = c.req.valid('json');
-
-      const shareHouse = await prisma.shareHouse.findUnique({
+      const sharehouse = await prisma.shareHouse.findUnique({
         where: {
           id: shareHouseId,
         },
         include: {
-          RotationAssignment: true,
+          RotationAssignment: {
+            select: {
+              rotationCycle: true,
+              categories: {
+                include: {
+                  tasks: true,
+                },
+              },
+              tenantPlaceholders: {
+                include: {
+                  tenant: true,
+                },
+              },
+            },
+          },
+          assignmentSheet: true,
         },
       });
 
-      if (!shareHouse) return c.json({ error: 'ShareHouse not found' }, 404);
+      /**
+       * Return 404 if sharehouse not found
+       */
+      if (!sharehouse) {
+        return c.json({ error: 'ShareHouse not found' }, 404);
+      }
 
-      if (!shareHouse.RotationAssignment)
-        return c.json({ error: 'Interval Server Error' }, 500);
+      /**
+       * Return 500 if RotationAssignment or AssignmentSheet not found
+       */
+      if (!sharehouse.RotationAssignment || !sharehouse.assignmentSheet) {
+        return c.json({ error: 'Internal Server Error' }, 500);
+      }
 
-      const updateRotationCycle = await prisma.rotationAssignment.update({
-        where: {
-          id: shareHouse.RotationAssignment.id,
-        },
-        data: {
-          rotationCycle: data.rotationCycle,
-        },
-      });
+      /**
+       * Create an AssignedData instance for the current rotation cycle.
+       */
+      const assignedData = new AssignedData(
+        sharehouse.assignmentSheet.assignedData as unknown as IAssignedData,
+        sharehouse.assignmentSheet.startDate,
+        sharehouse.assignmentSheet.endDate,
+      );
 
-      return c.json(updateRotationCycle, 201);
+      /**
+       * Generate the next rotation's assigned data.
+       */
+      const nextAssignedData = assignedData.createNextRotation(
+        sharehouse.RotationAssignment.categories,
+        sharehouse.RotationAssignment.tenantPlaceholders,
+        sharehouse.RotationAssignment.rotationCycle,
+      );
+
+      /**
+       * Structure the nextAssignedData to be returned.
+       */
+      const nextAssignmentData = {
+        name: sharehouse.name,
+        startDate: nextAssignedData.getStartDate().toISOString(),
+        endDate: addDays(nextAssignedData.getEndDate(), -1).toISOString(),
+        categories: nextAssignedData
+          .getAssignments()
+          .map((assignment) => {
+            if (!assignment.tenant) {
+              return null;
+            }
+
+            return {
+              id: assignment.category?.id ?? null,
+              name: assignment.category?.name ?? null,
+              tenant: {
+                id: assignment.tenant.id,
+                name: assignment.tenant.name,
+              },
+            };
+          })
+          .filter((assignment) => assignment !== null),
+      };
+
+      return c.json(nextAssignmentData);
     } catch (error) {
       console.error(error);
-      return c.json({ error: 'An error occurred while updating data' }, 500);
+      return c.json({ error: 'An error occurred while fetching data' }, 500);
     }
-  },
-);
+  })
+
+  /**
+   * Updates the rotation cycle of a sharehouse by its ID
+   * @route PATCH /api/rotation/:shareHouseId
+   */
+  .patch(
+    '/:shareHouseId',
+    zValidator('json', rotationCycleUpdateSchema),
+    async (c) => {
+      try {
+        const shareHouseId = c.req.param('shareHouseId');
+        const data = c.req.valid('json');
+
+        const shareHouse = await prisma.shareHouse.findUnique({
+          where: {
+            id: shareHouseId,
+          },
+          include: {
+            RotationAssignment: true,
+          },
+        });
+
+        if (!shareHouse) return c.json({ error: 'ShareHouse not found' }, 404);
+
+        if (!shareHouse.RotationAssignment)
+          return c.json({ error: 'Interval Server Error' }, 500);
+
+        const updateRotationCycle = await prisma.rotationAssignment.update({
+          where: {
+            id: shareHouse.RotationAssignment.id,
+          },
+          data: {
+            rotationCycle: data.rotationCycle,
+          },
+        });
+
+        return c.json(updateRotationCycle, 201);
+      } catch (error) {
+        console.error(error);
+        return c.json({ error: 'An error occurred while updating data' }, 500);
+      }
+    },
+  );
+
+export default app;
