e6a277ba5a5126e8a075ec1160699cbb450dc491
yuki-92c <jdpmcs@gmail.com>
2024-07-21 14:04:55 -0700

    feat: add validation for category

--- src/components/setup-sharehouse/SetupStepper.tsx
+++ src/components/setup-sharehouse/SetupStepper.tsx
@@ -15,6 +15,12 @@ import { TenantListItem } from '../ui/tenantList';
 import { DatePicker } from '@/components/ui/datePicker';
 import { SetupConfirmationDrawer } from '@/components/ui/drawers/SetupConfirmationDrawer';
 import { SetupContents } from '@/components/setup-sharehouse/SetupContents';
+import {
+  shareHouseCreationSchema,
+  TShareHouseCreationSchema,
+} from '@/constants/schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
 interface ISetupStepperProps {
   initialStep: number;
@@ -31,107 +37,181 @@ export const SetupStepper = ({
 }: ISetupStepperProps) => {
   const [currentStep, setCurrentStep] = useState<number>(initialStep);
   const [openDrawer, setOpenDrawer] = useState<boolean>(false);
-  const [shareHouseName, setShareHouseName] = useState<string>('');
-  const handleNext = () => setCurrentStep(currentStep + 1);
+  // const [shareHouseName, setShareHouseName] = useState<string>('');
+
   const handleBack = () => setCurrentStep(currentStep - 1);
   const handleOpen = () => setOpenDrawer(true);
 
-  switch (currentStep) {
-    case 1:
-      return (
-        <SetupContents
-          title="Create share house name"
-          currentStep={currentStep}
-          maxSteps={maxSteps}
-          onNext={handleNext}
-          isNextDisabled={!shareHouseName}
-        >
+  const {
+    register,
+    trigger,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<TShareHouseCreationSchema>({
+    resolver: zodResolver(shareHouseCreationSchema),
+    defaultValues: {
+      categories,
+      tenants,
+    },
+  });
+
+  const handleNext = async () => {
+    let isValid = false;
+    if (currentStep === 1) {
+      isValid = await trigger(['name']);
+    } else if (currentStep === 2) {
+      const values = getValues();
+      if (values.categories.length < 1 || values.categories.length > 15) {
+        return;
+      } else {
+        isValid = true;
+      }
+    } else if (currentStep === 3) {
+      isValid = await trigger(['tenants']);
+    } else if (currentStep === 4) {
+      isValid = await trigger(['startDate', 'rotationCycle']);
+    }
+    if (isValid) {
+      setCurrentStep((prevStep) => prevStep + 1);
+    }
+  };
+
+  const onSubmit = (data: TShareHouseCreationSchema) => {
+    console.log(data);
+  };
+
+  const shareHouseNameSetting = () => {
+    return (
+      <SetupContents
+        title="Share house name"
+        currentStep={currentStep}
+        maxSteps={maxSteps}
+        onNext={handleNext}
+      >
+        <div className="mb-6">
           <Input
-            placeholder="Sample share house"
-            value={shareHouseName}
-            onChange={(e) => setShareHouseName(e.target.value)}
+            label="Share house name"
+            placeholder="Enter share house name"
+            onError={() => errors.name?.message}
+            {...register('name')}
           />
-        </SetupContents>
-      );
-    case 2:
-      return (
-        <SetupContents
-          title="Create new category and task"
-          currentStep={currentStep}
-          maxSteps={maxSteps}
-          onNext={handleNext}
-          onBack={handleBack}
-        >
-          <ShareHouseManagementHead title="Categories" type="categories" />
-          {categories.map((category) => (
-            <Accordion
-              type="single"
-              collapsible
-              className="w-full"
-              key={category.id}
-            >
-              <AccordionItem value={`item-${category.id}`}>
-                <AccordionCategoryItem category={category} />
-                <AccordionContent className="space-y-4 bg-primary-lightest p-0">
-                  {category.tasks.map((task) => (
-                    <AccordionTaskItem
-                      key={task.id}
-                      id={task.id}
-                      category={category}
-                      title={task.title}
-                      description={task.description}
-                    />
-                  ))}
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          ))}
-        </SetupContents>
-      );
-    case 3:
-      return (
-        <SetupContents
-          title="Invite tenants"
-          currentStep={currentStep}
-          maxSteps={maxSteps}
-          onNext={handleNext}
-          onBack={handleBack}
-        >
-          <ShareHouseManagementHead title="Tenants" type="tenants" />
-          {tenants.length > 0 ? (
-            <ul className="mt-6">
-              {tenants.map((tenant) => (
-                <li className="mb-4" key={tenant.id}>
-                  <TenantListItem tenant={tenant} />
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p className="text-center">No tenant</p>
-          )}
-        </SetupContents>
-      );
-    case 4:
-      return (
-        <>
-          <SetupContents
-            title="Setup rotation cycle"
-            currentStep={currentStep}
-            maxSteps={maxSteps}
-            onNext={handleNext}
-            onBack={handleBack}
-            onOpen={handleOpen}
+        </div>
+        {errors.name?.message && (
+          <p className="text-red-500 text-sm">{errors.name?.message}</p>
+        )}
+      </SetupContents>
+    );
+  };
+
+  const categorySetting = () => {
+    return (
+      <SetupContents
+        title="Create new category and task"
+        currentStep={currentStep}
+        maxSteps={maxSteps}
+        onNext={handleNext}
+        onBack={handleBack}
+      >
+        <ShareHouseManagementHead title="Categories" type="categories" />
+        {categories.map((category) => (
+          <Accordion
+            type="single"
+            collapsible
+            className="w-full"
+            key={category.id}
           >
-            <div className="mb-6">
-              <p className="mb-6">Start date</p>
-              <DatePicker />
-            </div>
-            <RotationCycle />
-          </SetupContents>
-          <SetupConfirmationDrawer open={openDrawer} setOpen={setOpenDrawer} />
-        </>
-      );
-    default:
-      return <div>Invalid step</div>;
-  }
+            <AccordionItem value={`item-${category.id}`}>
+              <AccordionCategoryItem category={category} />
+              <AccordionContent className="space-y-4 bg-primary-lightest p-0">
+                {category.tasks.map((task) => (
+                  <AccordionTaskItem
+                    key={task.id}
+                    id={task.id}
+                    category={category}
+                    title={task.title}
+                    description={task.description}
+                  />
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ))}
+        {errors.categories && (
+          <p className="text-red-500 text-sm">{errors.categories.message}</p>
+        )}
+        {categories.length < 0 && (
+          <p className="text-center">{categories.length}</p>
+        )}
+      </SetupContents>
+    );
+  };
+
+  const tenantSetting = () => {
+    return (
+      <SetupContents
+        title="Invite tenants"
+        currentStep={currentStep}
+        maxSteps={maxSteps}
+        onNext={handleNext}
+        onBack={handleBack}
+      >
+        <ShareHouseManagementHead title="Tenants" type="tenants" />
+        {tenants.length > 0 ? (
+          <ul className="mt-6">
+            {tenants.map((tenant) => (
+              <li className="mb-4" key={tenant.id}>
+                <TenantListItem tenant={tenant} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-center">No tenant</p>
+        )}
+      </SetupContents>
+    );
+  };
+
+  const scheduleSetting = () => {
+    return (
+      <>
+        <SetupContents
+          title="Setup rotation cycle"
+          currentStep={currentStep}
+          maxSteps={maxSteps}
+          onNext={handleNext}
+          onBack={handleBack}
+          onOpen={handleOpen}
+        >
+          <div className="mb-6">
+            <p className="mb-6">Start date</p>
+            <DatePicker />
+          </div>
+          <RotationCycle />
+        </SetupContents>
+        <SetupConfirmationDrawer open={openDrawer} setOpen={setOpenDrawer} />
+      </>
+    );
+  };
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return shareHouseNameSetting();
+      case 2:
+        return categorySetting();
+      case 3:
+        return tenantSetting();
+      case 4:
+        return scheduleSetting();
+      default:
+        return <div>Invalid step</div>;
+    }
+  };
+
+  return (
+    <div className="w-full">
+      <form onSubmit={handleSubmit(onSubmit)}>{renderStep()}</form>
+    </div>
+  );
 };
