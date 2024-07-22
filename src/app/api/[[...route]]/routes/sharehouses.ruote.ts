d8be12fe211939b50047561cce7bdaf5bea989af
akio <124953279+akiomatic@users.noreply.github.com>
2024-07-22 14:20:25 -0700

    feat: add try-catch block in `/api/sharehouses` (#258)

--- src/app/api/[[...route]]/routes/sharehouses.ruote.ts
+++ src/app/api/[[...route]]/routes/sharehouses.ruote.ts
@@ -11,62 +11,73 @@ const app = new Hono()
    * @route GET /api/sharehouses
    */
   .get('/', async (c) => {
-    const session = await auth();
+    try {
+      const session = await auth();
 
-    if (!session) return c.json({ error: 'Unauthorized' }, 401);
+      if (!session) return c.json({ error: 'Unauthorized' }, 401);
 
-    const shareHouses = await prisma.landlord.findUnique({
-      where: { id: session.user.id },
-      include: {
-        shareHouses: {
-          include: {
-            assignmentSheet: {
-              select: {
-                assignedData: true,
+      const shareHouses = await prisma.landlord.findUnique({
+        where: { id: session.user.id },
+        include: {
+          shareHouses: {
+            include: {
+              assignmentSheet: {
+                select: {
+                  assignedData: true,
+                },
               },
             },
           },
         },
-      },
-    });
+      });
 
-    if (!shareHouses) return c.json({ error: 'Internal Server Error' }, 500);
+      if (!shareHouses) return c.json({ error: 'Internal Server Error' }, 500);
 
-    const shareHousesWithProgress = shareHouses.shareHouses.map(
-      (shareHouse) => {
-        const assignedData = shareHouse.assignmentSheet
-          .assignedData as unknown as IAssignedData;
+      const shareHousesWithProgress = shareHouses.shareHouses.map(
+        (shareHouse) => {
+          const assignedData = shareHouse.assignmentSheet
+            .assignedData as unknown as IAssignedData;
 
-        const isCompletedValues: boolean[] = [];
+          const isCompletedValues: boolean[] = [];
 
-        if (assignedData) {
-          for (const assignment of assignedData.assignments) {
-            if (assignment.tasks) {
-              for (const task of assignment.tasks) {
-                isCompletedValues.push(task.isCompleted);
+          if (assignedData) {
+            for (const assignment of assignedData.assignments) {
+              if (assignment.tasks) {
+                for (const task of assignment.tasks) {
+                  isCompletedValues.push(task.isCompleted);
+                }
               }
             }
           }
-        }
 
-        const completedValues = isCompletedValues.filter(
-          (value) => value === true,
-        );
-        const progressRate = Number(
-          ((completedValues.length / isCompletedValues.length) * 100).toFixed(
-            1,
-          ),
-        );
+          const completedValues = isCompletedValues.filter(
+            (value) => value === true,
+          );
+          const progressRate = Number(
+            ((completedValues.length / isCompletedValues.length) * 100).toFixed(
+              1,
+            ),
+          );
 
-        return {
-          id: shareHouse.id,
-          name: shareHouse.name,
-          progress: Number.isNaN(progressRate) ? 0 : progressRate,
-        };
-      },
-    );
+          return {
+            id: shareHouse.id,
+            name: shareHouse.name,
+            progress: Number.isNaN(progressRate) ? 0 : progressRate,
+          };
+        },
+      );
 
-    return c.json({ shareHouses: shareHousesWithProgress });
+      return c.json({ shareHouses: shareHousesWithProgress });
+    } catch (error) {
+      console.error(error);
+      return c.json(
+        {
+          error:
+            'Something went long while fetching all share houses the landlord has',
+        },
+        500,
+      );
+    }
   });
 
 export default app;
