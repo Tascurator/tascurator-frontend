004a7f0cf0fd872a9e923a323ecd2d286fcb2c12
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-07-24 16:49:20 -0700

    fix: fix middleware config for public images

--- src/middleware.ts
+++ src/middleware.ts
@@ -38,5 +38,10 @@ export default auth(async function middleware(req: NextRequest) {
 });
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
