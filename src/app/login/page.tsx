af37bd9e3fa27b881b9456bf8decfe32239e1630
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-07-24 16:50:07 -0700

    refactor: remove unnecessary comment

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,6 @@
 import { Form } from '@/components/login/Form';
 import { Logo } from '@/components/ui/Logo';
 
-/**
- * This login page was created just for backend development purposes.
- * TODO: Improve and implement the proper login page with the proper logic.
- */
 const LoginPage = () => {
   return (
     <div className={'flex flex-col px-6'}>
