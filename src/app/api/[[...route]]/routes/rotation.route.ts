1d03cca930ce3fe966f79dd4b56dd0660d716ce1
Kai <kkai.chien0813@gmail.com>
2024-07-26 10:28:12 -0700

    feat: add new endpoint

--- src/app/api/[[...route]]/routes/rotation.route.ts
+++ src/app/api/[[...route]]/routes/rotation.route.ts
@@ -8,7 +8,235 @@ import type { IAssignedData } from '@/types/server';
 import { AssignedData } from '@/services/AssignedData';
 import { addDays } from '@/utils/dates';
 
+type CategoryData = {
+  id: string | null;
+  name: string | null;
+  maxTasks: number | null;
+  completedTasks: number | null;
+  tenant: {
+    id: string;
+    name: string;
+  } | null;
+} | null;
+
+type RotationData = {
+  name: string;
+  startDate: string;
+  endDate: string;
+  progressRate: number | string;
+  categories: CategoryData[] | null;
+};
+
 const app = new Hono()
+
+  /**
+   * Retrieves both the current and next rotation cycle data of a sharehouse by its ID
+   * @route GET /api/rotation/:shareHouseId
+   */
+  .get('/:shareHouseId', async (c) => {
+    const shareHouseId = c.req.param('shareHouseId');
+    try {
+      const shareHouse = await prisma.shareHouse.findUnique({
+        where: {
+          id: shareHouseId,
+        },
+        include: {
+          assignmentSheet: true,
+          RotationAssignment: {
+            select: {
+              rotationCycle: true,
+              categories: {
+                include: {
+                  tasks: true,
+                },
+              },
+              tenantPlaceholders: {
+                include: {
+                  tenant: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      /**
+       * Return 404 if sharehouse not found
+       */
+      if (!shareHouse) {
+        return c.json(
+          {
+            error: SERVER_ERROR_MESSAGES.NOT_FOUND('share house'),
+          },
+          404,
+        );
+      }
+
+      /**
+       * Return 500 if RotationAssignment or AssignmentSheet not found
+       */
+      if (!shareHouse.RotationAssignment || !shareHouse.assignmentSheet) {
+        return c.json(
+          { error: SERVER_ERROR_MESSAGES.INTERNAL_SERVER_ERROR },
+          500,
+        );
+      }
+
+      // Current rotation data
+      const assignedData = shareHouse.assignmentSheet
+        .assignedData as unknown as IAssignedData;
+
+      const areAllTenantsNull = assignedData.assignments.every(
+        (assignment) => assignment.tenant === null,
+      );
+
+      let currentRotationData: RotationData;
+      if (areAllTenantsNull) {
+        currentRotationData = {
+          name: shareHouse.name,
+          startDate: shareHouse.assignmentSheet.startDate.toISOString(),
+          endDate: addDays(
+            shareHouse.assignmentSheet.endDate,
+            -1,
+          ).toISOString(),
+          progressRate: 0,
+          categories: null,
+        };
+      } else {
+        let totalTasks = 0;
+        let totalCompletedTasks = 0;
+
+        const missingTenant = assignedData.assignments.find(
+          (assignment) => !assignment.tenant,
+        );
+        if (missingTenant)
+          return c.json(
+            { error: SERVER_ERROR_MESSAGES.INTERNAL_SERVER_ERROR },
+            500,
+          );
+
+        const categories: CategoryData[] = assignedData.assignments.map(
+          (assignment) => {
+            const maxTasks = assignment.tasks ? assignment.tasks.length : null;
+            const completedTasks = assignment.tasks
+              ? assignment.tasks.filter((task) => task.isCompleted === true)
+                  .length
+              : null;
+
+            if (maxTasks !== null) totalTasks += maxTasks;
+            if (completedTasks !== null) totalCompletedTasks += completedTasks;
+
+            const categoryId = assignment.category
+              ? assignment.category.id
+              : null;
+            const categoryName = assignment.category
+              ? assignment.category.name
+              : null;
+
+            const tenantId = assignment.tenant!.id;
+            const tenantName = assignment.tenant!.name;
+
+            return {
+              id: categoryId,
+              name: categoryName,
+              maxTasks: maxTasks,
+              completedTasks: completedTasks,
+              tenant: {
+                id: tenantId,
+                name: tenantName,
+              },
+            };
+          },
+        );
+
+        const progressRate =
+          totalTasks > 0
+            ? ((totalCompletedTasks / totalTasks) * 100).toFixed(1)
+            : 0;
+
+        currentRotationData = {
+          name: shareHouse.name,
+          startDate: shareHouse.assignmentSheet.startDate.toISOString(),
+          endDate: addDays(
+            shareHouse.assignmentSheet.endDate,
+            -1,
+          ).toISOString(),
+          progressRate: progressRate,
+          categories: categories,
+        };
+      }
+
+      // Next rotation data
+      /**
+       * Create an AssignedData instance for the current rotation cycle.
+       */
+      const assignedDataInstance = new AssignedData(
+        shareHouse.assignmentSheet.assignedData as unknown as IAssignedData,
+        shareHouse.assignmentSheet.startDate,
+        shareHouse.assignmentSheet.endDate,
+      );
+
+      /**
+       * Generate the next rotation's assigned data.
+       */
+      const nextAssignedData = assignedDataInstance.createNextRotation(
+        shareHouse.RotationAssignment.categories,
+        shareHouse.RotationAssignment.tenantPlaceholders,
+        shareHouse.RotationAssignment.rotationCycle,
+      );
+
+      /**
+       * Structure the nextRotationData to be returned.
+       */
+      const nextRotationData = {
+        name: shareHouse.name,
+        startDate: nextAssignedData.getStartDate().toISOString(),
+        endDate: addDays(nextAssignedData.getEndDate(), -1).toISOString(),
+        categories: nextAssignedData
+          .getAssignments()
+          .map((assignment) => {
+            if (!assignment.tenant) {
+              return null;
+            }
+
+            return {
+              id: assignment.category?.id ?? null,
+              name: assignment.category?.name ?? null,
+              tenant: {
+                id: assignment.tenant.id,
+                name: assignment.tenant.name,
+              },
+            };
+          })
+          .filter((assignment) => assignment !== null) as CategoryData[],
+      };
+
+      /**
+       *  Combine and return both current and next rotation data.
+       */
+      const combinedData = {
+        current: currentRotationData,
+        next: nextRotationData,
+      };
+
+      return c.json(combinedData);
+    } catch (error) {
+      console.error(
+        SERVER_ERROR_MESSAGES.CONSOLE_COMPLETION_ERROR(
+          'fetching the data for the current rotation',
+        ),
+        error,
+      );
+      return c.json(
+        {
+          error: SERVER_ERROR_MESSAGES.COMPLETION_ERROR(
+            'fetching the data for the current rotation',
+          ),
+        },
+        500,
+      );
+    }
+  })
 
   /**
    * Retrieves a list of all categories with who they are assigned to for the current rotation cycle of a sharehouse by its ID
@@ -271,11 +499,13 @@ const app = new Hono()
           },
         });
 
-        if (!shareHouse)
-          return c.json(
-            { error: SERVER_ERROR_MESSAGES.NOT_FOUND('share house') },
-            404,
-          );
+        /**
+         * Return 404 if sharehouse not found
+         */
+        return c.json(
+          { error: SERVER_ERROR_MESSAGES.NOT_FOUND('share house') },
+          404,
+        );
 
         if (!shareHouse.RotationAssignment)
           return c.json(
