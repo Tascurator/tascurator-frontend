b09e4214c3ed96e71f1752346cfe653e2b0ec01d
akio <124953279+akiomatic@users.noreply.github.com>
2024-07-29 07:03:01 -0700

    feat: add more customizability to `CommonDrawer`

--- src/components/ui/commons/CommonDrawer.tsx
+++ src/components/ui/commons/CommonDrawer.tsx
@@ -5,53 +5,42 @@ import {
   DrawerTrigger,
 } from '@/components/ui/drawer';
 import { ReactNode } from 'react';
-import { FieldValues, SubmitHandler, UseFormReturn } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useFormContext } from 'react-hook-form';
 import { LoadingSpinner } from '@/components/ui/loadingSpinner';
 
-interface ICommonDrawerBaseProps<T extends FieldValues> {
+interface ICommonDrawer<T extends FieldValues> {
   title: string;
-  formControls: UseFormReturn<T>;
   open: boolean;
   setOpen: (open: boolean) => void;
   className?: string;
+  onSubmit: SubmitHandler<T> | null;
   children: ReactNode;
 }
 
-interface ICommonDrawerWithFormSubmissionProps<T extends FieldValues>
-  extends ICommonDrawerBaseProps<T> {
-  formSubmission: true;
-  onSubmit: SubmitHandler<T>;
-}
-
-type TCCommonDrawerProps<T extends FieldValues> =
-  | ICommonDrawerBaseProps<T>
-  | ICommonDrawerWithFormSubmissionProps<T>;
-
 export const CommonDrawer = <T extends FieldValues>({
   title,
-  formControls,
   open,
   setOpen,
   className,
+  onSubmit,
   children,
-  ...rest
-}: TCCommonDrawerProps<T>) => {
+}: ICommonDrawer<T>) => {
   const {
     formState: { isSubmitting },
     handleSubmit,
-  } = formControls;
+  } = useFormContext<T>();
 
   return (
     <>
       {/* Lading spinner if the loadingSpinner === true */}
-      {'formSubmission' in rest && <LoadingSpinner isLoading={isSubmitting} />}
+      {onSubmit && <LoadingSpinner isLoading={isSubmitting} />}
 
       {/* Drawer */}
       <Drawer open={open} onOpenChange={setOpen} modal={!isSubmitting}>
         <DrawerTrigger />
-        <DrawerContent className={className} asChild={'formSubmission' in rest}>
-          {'formSubmission' in rest ? (
-            <form onSubmit={handleSubmit(rest.onSubmit)}>
+        <DrawerContent className={className} asChild={!!onSubmit}>
+          {onSubmit ? (
+            <form onSubmit={handleSubmit(onSubmit)}>
               <DrawerTitle>{title}</DrawerTitle>
               {children}
             </form>
