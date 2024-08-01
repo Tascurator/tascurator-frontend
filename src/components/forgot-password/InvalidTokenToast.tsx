534cb21d520ab05bd22fdb5dec2eb89517841db2
akio <124953279+akiomatic@users.noreply.github.com>
2024-08-01 09:33:49 -0700

    fix: resolve with invalid token toast not displaying

--- src/components/forgot-password/InvalidTokenToast.tsx
+++ src/components/forgot-password/InvalidTokenToast.tsx
@@ -1,16 +1,24 @@
-import { useEffect } from 'react';
+'use client';
+
+import { useEffect, useState } from 'react';
 import { useToast } from '@/components/ui/use-toast';
 
 export const InvalidTokenToast = () => {
   const { toast } = useToast();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   useEffect(() => {
     toast({
       title: 'Invalid token',
-      description: 'The token you provided is invalid or has expired.',
+      description:
+        'The token is invalid or has expired. Please request a password reset again.',
       variant: 'destructive',
     });
-  }, []);
+  }, [mounted]);
 
-  return <></>;
+  return null;
 };
