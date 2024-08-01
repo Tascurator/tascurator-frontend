bb21650bca947a88e3c10924b61f8fd741d03500
akio <124953279+akiomatic@users.noreply.github.com>
2024-07-31 23:55:39 -0700

    feat: add `getPasswordResetDataByToken` method

---
+++ src/utils/prisma-helpers.ts
@@ -0,0 +1,37 @@
+import prisma from '@/lib/prisma';
+
+/**
+ * Get a landlord by email address
+ *
+ * @param email - The email address of the landlord
+ * @returns A promise that resolves with the landlord
+ */
+export const getLandlordByEmail = async (email: string) => {
+  try {
+    return await prisma.landlord.findUnique({
+      where: {
+        email,
+      },
+    });
+  } catch {
+    return null;
+  }
+};
+
+/**
+ * Get the associated password reset token data for a given token
+ *
+ * @param token - The token to search for
+ * @returns A promise that resolves with the password reset token data
+ */
+export const getPasswordResetDataByToken = async (token: string) => {
+  try {
+    return await prisma.passwordResetToken.findUnique({
+      where: {
+        token,
+      },
+    });
+  } catch {
+    return null;
+  }
+};
