c93d95161ad98e0621106e5fa25420c435946633
akio <124953279+akiomatic@users.noreply.github.com>
2024-08-01 00:43:06 -0700

    docs: add TSDoc

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -3,6 +3,14 @@ import { randomUUID } from 'node:crypto';
 import { CONSTRAINTS } from '@/constants/constraints';
 import { getPasswordResetTokenByEmail } from '@/utils/prisma-helpers';
 
+/**
+ * Generate a password reset token for the email address
+ *
+ * If there's an existing token for the email address, it will be deleted before creating a new one.
+ *
+ * @param email - The email address to generate the token for
+ * @returns A promise that resolves with the password reset token
+ */
 export const generatePasswordResetToken = async (email: string) => {
   const token = randomUUID();
   const expiresAt = new Date(
