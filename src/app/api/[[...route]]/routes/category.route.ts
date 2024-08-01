360727623f188073a5f4f5c8ef0ba5599de73d32
Kai <kkai.chien0813@gmail.com>
2024-08-01 09:09:11 -0700

    fix: fix the condition for category max limit

--- src/app/api/[[...route]]/routes/category.route.ts
+++ src/app/api/[[...route]]/routes/category.route.ts
@@ -197,7 +197,8 @@ const app = new Hono()
         }
 
         if (
-          rotationAssignment.categories.length > CONSTRAINTS.CATEGORY_MAX_AMOUNT
+          rotationAssignment.categories.length >=
+          CONSTRAINTS.CATEGORY_MAX_AMOUNT
         ) {
           return c.json(
             {
