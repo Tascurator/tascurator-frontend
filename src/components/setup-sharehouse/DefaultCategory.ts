9f68f8737de070f852f4eeee66a31eb3141e0d83
yuki-92c <jdpmcs@gmail.com>
2024-08-01 16:20:25 -0700

    feat: create DefaultCategory

---
+++ src/components/setup-sharehouse/DefaultCategory.ts
@@ -0,0 +1,56 @@
+export const DefaultCategory = [
+  {
+    id: 'bde250a4-116a-4641-b0f0-2718abf34bc5',
+    name: 'Kitchen',
+    tasks: [
+      {
+        id: 'b54f7377-b6c3-44df-8eeb-93fe10516c24',
+        title: 'Mop the floor',
+        description:
+          'Your task is to mop the floor. You can use the mop in the storage room.',
+      },
+      {
+        id: 'c3d177b8-b65e-454d-b65f-830c451a70ce',
+        title: 'Clean the oven',
+        description:
+          'Your task is to clean the oven. Make sure to use the oven cleaner and a sponge.',
+      },
+    ],
+  },
+  {
+    id: '5acfa1d1-1c52-4b49-b03d-c9781302bb39',
+    name: 'Bathroom',
+    tasks: [
+      {
+        id: '5c438fc9-e2d6-470a-bcae-53cbbff783fe',
+        title: 'Toilet',
+        description:
+          'Your task is to clean the toilet. Make sure to use the toilet brush and disinfectant.',
+      },
+      {
+        id: '504c0f05-297c-4118-9842-b6c81633a0dd',
+        title: 'Scrub the tiles',
+        description:
+          'Your task is to scrub the shower tiles. Use the tile cleaner and a brush to remove any soap scum and mildew.',
+      },
+    ],
+  },
+  {
+    id: '7a487d0e-e843-4478-984f-b1cb213a0647',
+    name: 'Liging Room',
+    tasks: [
+      {
+        id: '8747433e-afd4-4e41-9ae7-8847d2a5949d',
+        title: 'Vacuum the carpet',
+        description:
+          'Your task is to vacuum the carpet. Make sure to move the furniture and get into the corners.',
+      },
+      {
+        id: 'b353c7f9-a364-4ec8-8231-4ae276301c46',
+        title: 'Dust the shelves',
+        description:
+          'Your task is to dust the shelves. Use a microfiber cloth to remove any dust and dirt.',
+      },
+    ],
+  },
+];
