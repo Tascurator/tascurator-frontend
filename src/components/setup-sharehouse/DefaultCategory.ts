df70e65e63e01ef6b326631765df4379a9a6956d
yuki-92c <jdpmcs@gmail.com>
2024-08-13 22:05:45 -0700

    fix: fix the default category template

--- src/components/setup-sharehouse/DefaultCategory.ts
+++ src/components/setup-sharehouse/DefaultCategory.ts
@@ -1,58 +1,58 @@
 import { ICategory } from '@/types/commons';
-import { randomUUID } from 'crypto';
+import { generateRandomUUID } from '@/utils/genarate-uuid';
 
 export const DefaultCategory: ICategory[] = [
   {
-    id: randomUUID(),
+    id: generateRandomUUID(),
+    name: 'Washroom',
+    tasks: [
+      {
+        id: generateRandomUUID(),
+        title: 'Cleaning Toilet',
+        description:
+          '<ol class="list-decimal pl-6"><li class="[&amp;>p]:inline"><p>Use <u>a toilet brush</u> to scrub the inside of the toilet bowl.</p></li><li class="[&amp;>p]:inline"><p>Wipe down the seat, lid, and exterior with <u>disinfectant</u>.</p></li></ol><p></p><ul class="list-disc pl-6"><li class="[&amp;>p]:inline"><p>The toilet brush and the disinfectant are located in the cabinet under the sink.</p></li></ul>',
+      },
+      {
+        id: generateRandomUUID(),
+        title: 'Cleaning Mirror',
+        description:
+          '<ol class="list-decimal pl-6"><li class="[&amp;>p]:inline"><p>Spray <u>glass cleaner</u> on the mirror.</p></li><li class="[&amp;>p]:inline"><p>Wipe it clean with a <u>microfiber cloth</u>, ensuring no streaks are left.</p></li></ol><p></p><ul class="list-disc pl-6"><li class="[&amp;>p]:inline"><p>The glass cleaner and the microfiber cloth are located in the cabinet under the sink.</p><p></p></li></ul><p></p><p></p><p></p>',
+      },
+    ],
+  },
+  {
+    id: generateRandomUUID(),
     name: 'Kitchen',
     tasks: [
       {
-        id: randomUUID(),
-        title: 'Mop the floor',
+        id: generateRandomUUID(),
+        title: 'Cleaning Sink',
         description:
-          '<ul><li>Your task is to mop the floor. <u>You can use the mop in the storage room</u>.</li></ul>',
+          '<ol class="list-decimal pl-6"><li class="[&amp;>p]:inline"><p><strong>Remove debris from the sink</strong>.</p></li><li class="[&amp;>p]:inline"><p>Scrub the sink with a sponge and some dish soap.</p></li></ol>',
       },
       {
-        id: randomUUID(),
-        title: 'Clean the oven',
+        id: generateRandomUUID(),
+        title: 'Cleaning Fridge',
         description:
-          '<ul><li>Your task is to clean the oven. Make sure to use the oven cleaner and a sponge.</li></ul>',
+          '<ol class="list-decimal pl-6"><li class="[&amp;>p]:inline"><p>Remove <strong>expired items</strong> from the fridge.</p></li><li class="[&amp;>p]:inline"><p>Wipe down shelves and drawers with a <u>mild cleaner</u></p></li></ol><p></p><ul class="list-disc pl-6"><li class="[&amp;>p]:inline"><p>The mild cleaner is in the cupboard.</p></li></ul>',
       },
     ],
   },
   {
-    id: randomUUID(),
-    name: 'Bathroom',
-    tasks: [
-      {
-        id: randomUUID(),
-        title: 'Toilet',
-        description:
-          '<ul><li>Your task is to clean the toilet. Make sure to <b>use the toilet brush and disinfectant</b>.</li></ul>',
-      },
-      {
-        id: randomUUID(),
-        title: 'Scrub the tiles',
-        description:
-          '<ul><li>Your task is to scrub the shower tiles. Use the tile cleaner and a brush to remove any soap scum and mildew.</li></ul>',
-      },
-    ],
-  },
-  {
-    id: randomUUID(),
+    id: generateRandomUUID(),
     name: 'Liging Room',
     tasks: [
       {
-        id: randomUUID(),
-        title: 'Vacuum the carpet',
+        id: generateRandomUUID(),
+        title: 'Vacuuming the floor',
         description:
-          '<ul><li>Your task is to vacuum the carpet. Make sure to <b>move the furniture</b> and get into the corners.</li></ul>',
+          '<ol class="list-decimal pl-6"><li class="[&amp;>p]:inline"><p>Move small furniture out of the way.</p></li><li class="[&amp;>p]:inline"><p><u>Vacuum</u> the entire carpet, including corners and under furniture.</p><p></p></li></ol><p></p><ul class="list-disc pl-6"><li class="[&amp;>p]:inline"><p>The vacuum cleaner is stored in the closet near the entrance.</p></li></ul><p></p>',
       },
       {
-        id: randomUUID(),
-        title: 'Dust the shelves',
+        id: generateRandomUUID(),
+        title: 'Dusting',
         description:
-          '<ul><li>Your task is to dust the shelves. Use a <u>microfiber cloth</u> to remove any dust and dirt.</li></ul>',
+          '<ol class="list-decimal pl-6"><li class="[&amp;>p]:inline"><p>Use <u>a microfiber cloth</u> to dust all surfaces, including shelves and tables.</p></li><li class="[&amp;>p]:inline"><p>If the windows are dirty, clean them.</p></li></ol><p></p><ul class="list-disc pl-6"><li class="[&amp;>p]:inline"><p>The microfiber cloth is located in the cupboard in the kitchen.</p></li></ul>',
       },
     ],
   },
