4a1ffc9772ea614a9ccdb8b4327ff531dbdf5860
akio <124953279+akiomatic@users.noreply.github.com>
2024-08-20 22:54:02 -0700

    chore: correct icon names

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -19,8 +19,8 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: 'any',
         type: 'image/x-icon',
       },
-      { src: '/icon-192x192.png', type: 'image/png', sizes: '192x192' },
-      { src: '/icon-512x512.png', type: 'image/png', sizes: '512x512' },
+      { src: '/icon1.png', type: 'image/png', sizes: '192x192' },
+      { src: '/icon2.png', type: 'image/png', sizes: '512x512' },
     ],
   };
 }
